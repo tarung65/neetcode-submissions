591fe791049a056783b1fae3644ab1b7de540b31
tarung65 <tarung65@gmail.com>
2026-07-30 01:33:59 -0700

    Time: 175 ms (33.75%) | Memory: 69.8 MB (20.21%) - LeetSync

---
+++ 1928-number-of-orders-in-the-backlog/number-of-orders-in-the-backlog.cpp
@@ -0,0 +1,52 @@
+class Solution {
+    using ORDER = std::tuple<int,int,int>;
+    std::priority_queue<ORDER> buyorder;
+    std::priority_queue<ORDER,std::vector<ORDER>,std::greater<ORDER>> sellOrder;
+    const int MOD = 1e9 + 7;
+public:
+    int getNumberOfBacklogOrders(vector<vector<int>>& orders) {
+        for(auto o: orders){
+            if(o[2] == 0){ //buy order
+                while(!sellOrder.empty() && std::get<0>(sellOrder.top()) <= o[0] && o[1] > 0){
+                    auto [price, amount, orderType] = sellOrder.top();
+                    sellOrder.pop();
+                    if(amount <= o[1]){ //sell order is smaller than buy order
+                        o[1] -= amount;
+                    } else {//sell order is larger than buy order
+                        amount -= o[1];
+                        o[1] = 0;
+                        sellOrder.push({price, amount, orderType});
+                        break;
+                    }
+                }
+                if(o[1] > 0) buyorder.push({o[0], o[1], o[2]});
+            } else { //sell order
+                while(!buyorder.empty() && std::get<0>(buyorder.top()) >= o[0] && o[1] > 0){
+                    auto [price, amount, orderType] = buyorder.top();
+                    buyorder.pop();
+                    if(amount <= o[1]){ //buy order is smaller than sell order
+                        o[1] -= amount;
+                    } else {//buy order is larger than sell order
+                        amount -= o[1];
+                        o[1] = 0;
+                        buyorder.push({price, amount, orderType});
+                        break;
+                    }
+                }
+                if(o[1] > 0) sellOrder.push({o[0], o[1], o[2]});
+            }
+        }
+        long long backlog = 0;
+        while(!buyorder.empty()){
+            auto [price, amount, orderType] = buyorder.top();
+            backlog += amount;
+            buyorder.pop();
+        }
+        while(!sellOrder.empty()){
+            auto [price, amount, orderType] = sellOrder.top();
+            backlog += amount;
+            sellOrder.pop();
+        }
+        return backlog % MOD;
+    }
+};
